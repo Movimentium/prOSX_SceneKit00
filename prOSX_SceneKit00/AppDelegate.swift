de46507d3aab53ec6a1db8a3c861f54dad627c24
Miguel Gallego Martín <miguel.0.1.1.2.3.5.8@gmail.com>
2019-06-16 00:32:54 +0200

    Initial Commit

---
+++ prOSX_SceneKit00/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  prOSX_SceneKit00
+//
+//  Created by Miguel Gallego Martín on 16/06/2019.
+//  Copyright © 2019 Miguel Gallego Martín. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ prOSX_SceneKit00/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  prOSX_SceneKit00
+//
+//  Created by Miguel Gallego Martín on 16/06/2019.
+//  Copyright © 2019 Miguel Gallego Martín. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
